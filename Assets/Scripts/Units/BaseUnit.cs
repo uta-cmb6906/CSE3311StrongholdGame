8adc7b1265c4c3dec7dae98fd5a3d1e88e9c540f
Devin M <devinkellymackay@gmail.com>
2025-10-05 18:52:57 -0500

    Unit health bar and economic actions

--- Assets/Scripts/Units/BaseUnit.cs
+++ Assets/Scripts/Units/BaseUnit.cs
@@ -1,22 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BaseUnit : MonoBehaviour
 {
+    [SerializeField] private Image HealthBar;
+
     public Tile OccupiedTile;
     public bool isPlayer;
-    [SerializeField] protected float health;
+    protected int maxHealth;
+    [SerializeField] protected int health;
     [SerializeField] protected int defense;
     [SerializeField] protected int movementRange;
     [SerializeField] protected int attackRange;
     [SerializeField] protected int meleeDamage;
     [SerializeField] protected int rangedDamage;
 
-    public float Health() => health;
+    public int Health() => health;
     public int Defense() => defense;
     public int MovementRange() => movementRange;
     public int AttackRange() => attackRange;
     public int MeleeDamage() => meleeDamage;
     public int RangedDamage() => rangedDamage;
+
+    void Start()
+    {
+        maxHealth = health;
+        if (HealthBar != null) HealthBar.fillAmount = 1f;
+        else Debug.LogError("HealthBar not assigned!");
+
+        //TODO apply special faction modifiers
+    }
 
     //try to move to tile returning true if successful or false if tile out of range
     public bool AttemptMovement(Tile destination)
@@ -52,7 +65,7 @@ public class BaseUnit : MonoBehaviour
     {
         //enemy defense modified by its terrain bonus
         float enemyDefense = enemy.Defense() * enemy.OccupiedTile.TerrainModifier();
-        
+
         //if enemy is adjacent do melee attack
         if (InRange(enemy.OccupiedTile, 1))
         {
@@ -67,8 +80,41 @@ public class BaseUnit : MonoBehaviour
     //unit recieves specified damage modified by randomness and reduction. If its health is reduced to 0 it is destroyed
     private void TakeDamage(float damage, float reduction)
     {
-        float variation = Random.Range(.9f, 1.1f);
-        health -= damage * variation * (100 - reduction) / 100;
+        float variation = Random.Range(.8f, 1.2f);
+        health -= (int)(damage * variation * (100 - reduction) / 100);
+        UpdateHealthBar();
         if (health <= 0) Destroy(gameObject);
+    }
+
+    //restore unit health
+    public void HealUnit()
+    {
+        health = maxHealth;
+    }
+
+    //increase all unit stats by 20%
+    public void UpgradeUnit()
+    {
+        maxHealth = (int)maxHealth * 1.2f;
+        health = (int)health * 1.2f;
+        defense = (int)defense * 1.2f;
+        meleeDamage = (int)meleeDamage * 1.2f;
+        rangedDamage = (int)rangedDamage * 1.2f;
+    }
+
+    private void UpdateHealthBar()
+    {
+        HealthBar.fillAmount = (float)health / maxHealth;
+    }
+
+    public string UnitInfo()
+    {
+        return this.GetType().Name
+            + "\n+ " + health + " Health"
+            + "\n+ " + defense + " Defense"
+            + "\n+ " + movementRange + " Movement Range"
+            + "\n+ " + attackRange + " Attack Range"
+            + "\n+ " + meleeDamage + " Melee Damage"
+            + "\n+ " + rangedDamage + " Ranged Damage";
     }
 }
