e2bd0563834f01e3bb13c7e0854af9a3a9ab4fa9
sxknnn <sxk4940@mavs.uta.edu>
2025-10-21 17:24:31 -0500

    Difficulty window added in the main menu

--- Assets/Scripts/PopUpWindow/MainMenu.cs
+++ Assets/Scripts/PopUpWindow/MainMenu.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    [Header("Menu Panels")]
+    public GameObject mainMenuPanel;
+    //public GameObject optionsPanel;
+    public GameObject difficultyPanel;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -12,5 +18,38 @@ public class MainMenu : MonoBehaviour
     {
 
         Application.Quit();
+        Debug.Log("Quitting...");
+    }
+
+    // public void OpenOptions()
+    // {
+    //     mainMenuPanel.SetActive(false);
+    //     optionsPanel.SetActive(false);
+    //     difficultyPanel.SetActive(false);
+    // }
+
+    // public void CloseOptions()
+    // {
+    //     optionsPanel.SetActive(false);
+    //     mainMenuPanel.SetActive(true);
+    // }
+
+    public void OpenDifficultySettings()
+    {
+        mainMenuPanel.SetActive(false);
+        difficultyPanel.SetActive(true);
+    }
+
+    public void CloseDifficultySettings()
+    {
+        difficultyPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
+    // all the difficulty settings logic will implement it later
+    public void SetDifficulty(string difficultyLevel)
+    {
+        Debug.Log("Difficulty is set to " + difficultyLevel);
+        CloseDifficultySettings();
     }
 }
